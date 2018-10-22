01587ff60c90d53fb14351cf3180206295370a5e
Parham Alvani <parham.alvani@gmail.com>
2018-10-23 01:20:08 +0330

    Adds basis of proxy server.

---
+++ src/ir/ac/aut/ceit/udpproxy/proxy/Server.java
@@ -0,0 +1,58 @@
+package ir.ac.aut.ceit.udpproxy.proxy;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class Server extends Thread {
+    private DatagramSocket socket;
+    private boolean running;
+    private byte[] buf = new byte[256]; // maximum buffer size is 256
+
+    /**
+     * Creates new proxy server that listens on 1373/udp and binds on loopback interface
+     * TODO: bind on all interfaces
+     */
+    public Server() {
+        try {
+            socket = new DatagramSocket(1373, InetAddress.getLoopbackAddress());
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        running = true;
+
+        while (running) {
+            // in each iteration waits for new packet then handles it
+            DatagramPacket packet
+                    = new DatagramPacket(buf, buf.length);
+            try {
+                socket.receive(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            InetAddress address = packet.getAddress();
+            int port = packet.getPort(); // finds sender port in order to answer on
+            packet = new DatagramPacket(buf, buf.length, address, port); // answer packet
+
+            // proxies the request to its destination. finds destination from Host option of HTTP packets.
+            String received
+                    = new String(packet.getData(), 0, packet.getLength());
+
+           socket.send(packet);
+        }
+
+        socket.close();
+    }
+
+    @Override
+    public void interrupt() {
+        super.interrupt();
+        this.running = false;
+    }
+}
